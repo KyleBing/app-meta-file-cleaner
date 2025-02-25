3cd7d83d219d6de80269ced819a8f675dce8da31
KyleBing(MBP14) <kylebing@163.com>
2025-02-25 22:08:39 +0800

    add bg, finish shell script

--- MetaCleaner/launch.sh
+++ MetaCleaner/launch.sh
@@ -1,36 +1,100 @@
 #!/bin/sh
 
+find_files_in_sdcard() {
+    bg_deleting="./bg_deleting.png"
+    bg_confirm="./bg_confirm.png"
+    bg_searching_sdcard="./bg_searching_sdcard.png"
+    font="/mnt/SDCARD/Themes/Cozy/Galvji.ttf"
 
-remove_all_macos_meta_file() {
-    bg_ongoing="./bg_icon.png"
+    file_count=0
 
-    for folder in "/mnt/SDCARD/Apps/" "/mnt/SDCARD/Roms/" ; do
-        find folder -regex ".*\._.*" -exec rm -Rf {} \;
+    # Show searching bg
+    /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_searching_sdcard}
 
-        # count the number of files
-        file_count=`find . -regex ".*\._.*" -printf x | wc -c`
+    ## For single location
+    file_count=$(find "/mnt/SDCARD/" -regex ".*\._.*" | wc -l)
 
-        if [ -f "${apps_folder}/${icon_path}" ]; then
-            if (cp "${source_folder}/${icon_path}" "${apps_folder}/${icon_path}"); then
-                /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_ongoing} -m "SUCCESS - ${apps_folder}/${icon_path}"
-            fi
-        else
-            /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -c "255,59,48" -m "Fail - ${apps_folder}/${icon_path}  is not exist"
-        fi
-    done
+    button=$(/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_confirm} -c "220,220,220" -k "A B" -m "The number of files ( ._xxx ): ${file_count}" -ff ${font} -fs 40)
+
+    if [ "$button" = "A" ]; then
+        delete_files_in_sdcard
+        /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_exit}
+        exit 0
+
+    elif [ "$button" = "B" ]; then
+        /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_exit}
+        exit 0
+    fi
 }
 
+# delete all ._ files
+delete_files_in_sdcard() {
+    bg_deleting="./bg_deleting.png"
+    bg_confirm="./bg_confirm.png"
+    font="/mnt/SDCARD/Themes/Cozy/Galvji.ttf"
+
+    /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_deleting}
+    find "/mnt/SDCARD/" -regex ".*\._.*" -exec rm -Rf {} \;
+}
+
+find_files_in_common_folder() {
+    bg_deleting="./bg_deleting.png"
+    bg_confirm="./bg_confirm.png"
+    bg_searching_common="./bg_searching_common.png"
+    font="/mnt/SDCARD/Themes/Cozy/Galvji.ttf"
+
+    file_count=0
+
+    # Show searching bg
+    /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_searching_common}
+
+    ## For multiple location
+    for folder in "/mnt/SDCARD/Apps/" "/mnt/SDCARD/Roms/" "/mnt/SDCARD/Backgrounds/" "/mnt/SDCARD/Icons/" "/mnt/SDCARD/Themes/"; do
+
+        # count the number of files
+        count=$(find ${folder} -regex ".*\._.*" | wc -l)
+
+        let file_count+=count
+    done
+
+    button=$(/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_confirm} -c "220,220,220" -k "A B" -m "The number of files ( ._xxx ): ${file_count}" -ff ${font} -fs 40)
+
+    if [ "$button" = "A" ]; then
+        delete_files_in_common_folder
+        /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_exit}
+        exit 0
+
+    elif [ "$button" = "B" ]; then
+        /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_exit}
+        exit 0
+    fi
+}
+
+# delete all ._ files
+delete_files_in_common_folder() {
+    bg_deleting="./bg_deleting.png"
+    bg_confirm="./bg_confirm.png"
+    font="/mnt/SDCARD/Themes/Cozy/Galvji.ttf"
+
+    /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_deleting}
+    for folder in "/mnt/SDCARD/Apps/" "/mnt/SDCARD/Roms/" "/mnt/SDCARD/Backgrounds/" "/mnt/SDCARD/Icons/" "/mnt/SDCARD/Themes/"; do
+        find ${folder} -regex ".*\._.*" -exec rm -Rf {} \;
+    done
+}
 
 main() {
     bg="./instruction.png"
     bg_exit="./bg_exit.png"
 
-    button=$(/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i $bg -c "200,200,200" -k "A B X Y SELECT MENU")
-
+    button=$(/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i $bg -c "220,220,220" -k "A B Y")
 
     if [ "$button" = "A" ]; then
-        remove_all_macos_meta_file
+        find_files_in_common_folder
+        /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_exit}
+        exit 0
 
+    elif [ "$button" = "Y" ]; then
+        find_files_in_sdcard
         /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i ${bg_exit}
         exit 0
 
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+<img src="./MetaCleaner/icon.png" width="100" alt="logo">
+
 # macOS meta file cleaner
 
-For retro handheld runs linux.
+Find and remove all files starting with "._*" from the SD card.
+
+![find all files](./MetaCleaner/instruction.png)
+
+## Compatible
+
+- [x] CrossMix OS `2025-02-25`
+
+
+## Usage
+
+1. Download this.
+2. Put **MetaCleaner** into folder `/SDCARD/Apps/`
+3. Reboot device, you will see this app appears in the app list, open it.
+4. Press <kbd>A</kbd> to find any file that name starts with `._`. Press <kbd>A</kbd> again will delete all of them.
